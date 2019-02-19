718fe7d575e20cd770f5ca33798ddcc3bcdb658a
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-02-19 15:35:27 +0900

    headlineのatomを準備

---
+++ components/atoms/HeadLine.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div.headline
+    div
+      img(src="~/assets/images/title-mark.svg")
+    slot
+    div
+      img(src="~/assets/images/title-mark.svg")
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeadLine extends Vue {}
+</script>
+<style lang="scss" scoped>
+.headline {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
   div.header
-    | ヘッダー
-    GlobalNavi
+    div.navi
+      | ヘッダー
+      GlobalNavi
+    HeadLine
+      h1 div
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import GlobalNavi from '~/components/molecules/GlobalNavi.vue'
+import HeadLine from '~/components/atoms/HeadLine.vue'
 
 @Component({
   components: {
-    GlobalNavi
+    GlobalNavi,
+    HeadLine
   }
 })
 export default class Header extends Vue {}
@@ -19,5 +24,10 @@ export default class Header extends Vue {}
 <style scoped lang="scss">
 .header {
   background-color: red;
+
+  .navi {
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
