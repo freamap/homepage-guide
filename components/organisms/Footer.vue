0c43512c99b84948e5254cd35ef37653132b5787
YurikoYatoh <yuriko.yatoh@freamap.co.jp>
2019-02-19 16:59:28 +0900

    footer copyright作成

--- components/organisms/Footer.vue
+++ components/organisms/Footer.vue
@@ -1,6 +1,14 @@
 <template lang="pug">
-  div フッター
-    FooterLinks
+  footer.footer
+    div.logo
+      img(src="~/assets/images/title-mark.svg")
+    div.contents
+      div
+        FooterLinks
+      div.copyright
+        | Copyright© freamap ホームページ制作入門 , 2019 All Rights Reserved.
+    div.logo
+      img(src="~/assets/images/title-mark.svg")
 </template>
 
 <script lang="ts">
@@ -14,3 +22,33 @@ import FooterLinks from '~/components/molecules/FooterLinks.vue'
 })
 export default class Footer extends Vue {}
 </script>
+
+<style scoped lang="scss">
+.footer {
+  display: flex;
+  justify-content: center;
+  color: #236AF7;
+  background-color: #FFFFFF;
+  height: 107px;
+  margin-top: 23px;
+  margin-bottom: 30px;
+}
+
+.contents {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 40px;
+  margin-left: 40px;
+}
+
+.copyright {
+  font-size: 12px;
+  font-weight: bold;
+  margin-top: 12px;
+}
+
+.logo {
+  width: 56px;
+}
+</style>
