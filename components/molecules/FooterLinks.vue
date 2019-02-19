ea3c96d905cf8e594956afe4f57df1b538a4f470
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-02-19 15:42:28 +0900

    フッターリンク作成

--- components/molecules/FooterLinks.vue
+++ components/molecules/FooterLinks.vue
@@ -6,5 +6,16 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class FooterLinks extends Vue {}
+export default class FooterLinks extends Vue {
+  pages!: {
+    company: {
+      link: '/company',
+      title: '運営会社'
+    },
+    privacy: {
+      link: '/privacy',
+      title: 'プライバシーポリシー'
+    }
+  }
+}
 </script>
--- components/organisms/Footer.vue
+++ components/organisms/Footer.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
   div フッター
+    FooterLinks
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import GlobalNavi from '~/components/molecules/GlobalNavi.vue'
+import FooterLinks from '~/components/molecules/FooterLinks.vue'
 
 @Component({
   components: {
-    GlobalNavi
+    FooterLinks
   }
 })
 export default class Footer extends Vue {}
